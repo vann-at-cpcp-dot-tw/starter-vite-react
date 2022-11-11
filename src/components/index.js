d9f404f6ec0240a6265d09d3fb496f7954906929
Vann Liu <vann@cpcptw>
2022-11-11 11:56:12 +0800

    Fix: path filter

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,7 @@ const modules = import.meta.glob('@src/components/**/*.jsx')
 const components = Object.entries(modules || {}).reduce((acc, entry, index)=>{
   const path = entry[0]
   const importFunc = entry[1]
-  const name = toCamelCase(path.replaceAll('./', '').replaceAll('.jsx', '').replaceAll('/', ''), '/', true)
+  const name = toCamelCase(path.replaceAll('/src/components/', '').replaceAll('.jsx', '').replaceAll('/', ''), '/', true)
   return {
     ...acc,
     [name]: lazy(importFunc)
